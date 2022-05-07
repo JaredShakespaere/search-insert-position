9d9154d64ae78f57420f4a4075f2a80442b5f0fc
JaredShakespaere <90650932+JaredShakespaere@users.noreply.github.com>
2022-05-07 16:13:42 -0500

    full algo completed

--- searchInserPosition.js
+++ searchInserPosition.js
@@ -2,6 +2,7 @@ var searchInsert = function (nums, target) {
 	let pivot = target;
 	let listL = [];
 	let listR = [];
+	let listTar = [target];
 
 	for (let i = 0; i < nums.length; i++) {
 		if (pivot === nums[i]) {
@@ -13,5 +14,7 @@ var searchInsert = function (nums, target) {
 		}
 	}
 
-	console.log(`listL: ${listL}, listR: ${listR}`);
+	let fullList = listL.concat(listTar).concat(listR);
+
+	return fullList.indexOf(pivot);
 };
